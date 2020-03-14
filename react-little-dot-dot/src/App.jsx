e9b865b00b55c80e5abf83a134e9bd43f95d3df0
luoquanquan <luoquanquan@baidu.com>
2020-03-14 21:49:27 +0800

    setSate 时而同步, 时而异步

--- react-little-dot-dot/src/App.jsx
+++ react-little-dot-dot/src/App.jsx
@@ -1,50 +1,39 @@
 import React from 'react'
 
-const ThemeContext = React.createContext('light')
-
-const FuncChild = () => <ThemeContext.Consumer>
-    {context => <div>i'm func Child ~ current theme {context}</div>}
-</ThemeContext.Consumer>
-class Child extends React.Component {
-    constructor() {
-        super()
-        console.log('constructor')
-    }
-
-    static contextType = ThemeContext
-
-    render() {
-        const {context} = this
-        return <div>i'm child ~ current theme: {context}</div>
-    }
-}
 class App extends React.Component {
     constructor() {
         super()
-
-        this.state = {
-            theme: 'light'
-        }
-
+        this.state = { count: 0 }
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick() {
-        this.setState(({theme}) => ({
-            theme: theme === 'light' ? 'black' : 'light'
-        }))
+        this.setState(({count}) => ({count: count + 1}))
+        console.log(this.state.count)
+        this.setState(({count}) => ({count: count + 1}))
+        console.log(this.state.count)
+
+        setTimeout(() => {
+            this.setState(({count}) => ({count: count + 1}))
+            console.log(this.state.count)
+            this.setState(({count}) => ({count: count + 1}))
+            console.log(this.state.count)
+        }, 1e3);
     }
 
+    componentDidMount() {
+        this.btn2.addEventListener('click', this.handleClick)
+    }
+
+
     render() {
-        const {theme} = this.state
+        const {count} = this.state
         return (
-            <ThemeContext.Provider className="Provider" value={theme}>
-                <div className="app">
-                    <button onClick={this.handleClick}>click Me</button>
-                    <Child />
-                    <FuncChild />
-                </div>
-            </ThemeContext.Provider>
+            <div className="app">
+                <button onClick={this.handleClick}>click Me</button>
+                <button ref={ref => {this.btn2 = ref}}>click Me2</button>
+                {count}
+            </div>
         )
     }
 }
