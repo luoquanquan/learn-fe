5d971e08b45d0c781dc552410e03f9e9e4ac752c
luoquanquan <luoquanquan@baidu.com>
2020-03-14 18:16:14 +0800

    使用箭头函数解决 this 指向问题

--- react-little-dot-dot/src/App.jsx
+++ react-little-dot-dot/src/App.jsx
@@ -2,22 +2,8 @@ import React from 'react';
 
 class App extends React.Component {
 
-    handleClick(e) {
-        // 事件对象 e 是经过处理的一个实例
-        console.log(e)
-
-        // 事件对象所属的类
-        console.log(e.__proto__.constructor)
-
-        // e.target 当前触发事件的元素
-        // e.currentTarget 绑定事件的元素
-        console.log('e.target', e.target)
-        console.log('e.currentTarget', e.currentTarget)
-
-        // 原生事件对象
-        const {nativeEvent} = e
-        console.log('nativeEvent.target', nativeEvent.target)
-        console.log('nativeEvent.currentTarget', nativeEvent.currentTarget)
+    handleClick = () => {
+        console.log(this)
     }
 
     render() {
