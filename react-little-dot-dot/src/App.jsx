97d6111b6a2886c55143181dd08e364f1b017bbb
luoquanquan <luoquanquan@baidu.com>
2020-03-14 20:46:08 +0800

    生命周期函数

--- react-little-dot-dot/src/App.jsx
+++ react-little-dot-dot/src/App.jsx
@@ -1,19 +1,65 @@
 import React from 'react';
 
+class Child extends React.Component {
+    constructor() {
+        super()
+        console.log('constructor')
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount')
+    }
+
+    shouldComponentUpdate() {
+        console.log('shouldComponentUpdate')
+        return true
+    }
+
+    componentWillReceiveProps() {
+        console.log('componentWillReceiveProps')
+    }
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate')
+    }
+
+    render() {
+        console.log('render')
+
+        return <div>i'm child ~</div>
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+    }
+}
+
 class App extends React.Component {
     constructor() {
         super()
+
+        this.state = {
+            count: 0
+        }
+
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick() {
-        console.log(this)
+        this.setState(({count}) => ({
+            count: count + 1
+        }))
     }
 
     render() {
         return (
             <div className="app">
                 <button onClick={this.handleClick}>click Me</button>
+                <Child />
             </div>
         )
     }
