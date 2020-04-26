6f0a1378d62b622d9a6f9571bc22c83712316964
luoquanquan <luoquanquan@baidu.com>
2020-04-26 15:22:48 +0800

    实时获取 state 中的值没有问题

--- hook-learn/src/app.jsx
+++ hook-learn/src/app.jsx
@@ -2,10 +2,17 @@ import React, {useState} from 'react'
 
 const Count = () => {
     const [count, setCount] = useState(0)
+
+    const logCount = () => {
+        console.log(count)
+    }
+
     return (
         <>
             <div className="count">{count}</div>
+            <br/>
             <button onClick={() => setCount(count + 1)}>+1</button>
+            <button onClick={logCount}>logCount</button>
         </>
     )
 }
