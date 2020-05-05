d3b1dbaccb00220a471cdcbc26b50f148f943352
luoquanquan <luoquanquan@baidu.com>
2020-05-05 19:07:00 +0800

    使用自定义 hook 实现公共逻辑的封装-只复用逻辑, 不复用状态

--- hook-learn/src/customHook.jsx
+++ hook-learn/src/customHook.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
-const Count1 = () => {
+const useCount = () => {
     const [number, setNumber] = useState(0)
 
     useEffect(() => {
@@ -8,24 +8,23 @@ const Count1 = () => {
         return () => clearInterval(timer)
     })
 
+    return number
+}
+
+const Count1 = () => {
+    const number = useCount()
     return (
         <>
-            <div>当前: {number}</div>
+            <div>当前1: {number}</div>
         </>
     )
 }
 
 const Count2 = () => {
-    const [number, setNumber] = useState(0)
-
-    useEffect(() => {
-        const timer = setInterval(() => setNumber(n => n + 1), 1e3)
-        return () => clearInterval(timer)
-    })
-
+    const number = useCount()
     return (
         <>
-            <div>当前: {number}</div>
+            <div>当前2: {number}</div>
         </>
     )
 }
