f1adfce00bee0a26ba3334471b4a98b5d951f556
luoquanquan <luoquanquan@baidu.com>
2020-05-06 15:48:31 +0800

    模拟 redux 各种中间件

--- hook-learn/src/useReducer.jsx
+++ hook-learn/src/useReducer.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useReducer} from 'react'
+import React, {useState, useCallback, useReducer, useEffect} from 'react'
 
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
@@ -27,10 +27,49 @@ const useMyState = initialState => {
     return [state, setState]
 }
 
-const Count = () => {
+// 实现 redux-logger 在组件状态变更后打印新的状态值
+const useLogger = (reducer, initialState) => {
     const [state, dispatch] = useReducer(reducer, initialState)
+    const loggerDispatch = action => {
+        console.log('old state', state)
+        dispatch(action)
+    }
+    useEffect(() => console.log('new state', state))
+    return [state, loggerDispatch]
+}
+
+// 实现 thunk
+const useThunk = (reducer, initialState) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const thunkDispatch = action => {
+        if (typeof action === 'function') {
+            action(thunkDispatch, () => state)
+        } else {
+            dispatch(action)
+        }
+    }
+    return [state, thunkDispatch]
+}
+
+// 实现 usePromise
+const usePromise = (reducer, initialState) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const promiseDispatch = action => {
+        if (typeof action.then === 'function') {
+            action.then(promiseDispatch)
+        } else {
+            dispatch(action)
+        }
+    }
+    return [state, promiseDispatch]
+}
+
+const Count = () => {
+    const [state, dispatch] = useLogger(reducer, initialState)
     const [state2, setState] = useState(initialState)
     const [state3, setMyState] = useMyState(initialState)
+    const [state4, dispatch4] = useThunk(reducer, initialState)
+    const [state5, dispatch5] = usePromise(reducer, initialState)
 
     console.log('render Count ~')
     return (
@@ -49,6 +88,30 @@ const Count = () => {
             <div>{state3.number}</div>
             <button onClick={() => setMyState({number: state3.number + 1})}>+</button>
             <button onClick={() => setMyState({number: state3.number - 1})}>-</button>
+            <br/><br/>
+            <h3>useThunk</h3>
+            <div>{state4.number}</div>
+            <button onClick={() => dispatch4((dispatch, getState) => {
+                console.log(`当前时间 ${Date.now()}: debug 的数据是 getState(): `, getState())
+                dispatch({type: INCREMENT})
+            })}>+</button>
+            <button onClick={() => dispatch4((dispatch, getState) => {
+                console.log(`当前时间 ${Date.now()}: debug 的数据是 getState(): `, getState())
+                dispatch({type: DECREMENT})
+            })}>-</button>
+            <br/><br/>
+            <h3>usePromise</h3>
+            <div>{state5.number}</div>
+            <button onClick={() => dispatch5(new Promise(resolve => {
+                setTimeout(() => {
+                    resolve({type: INCREMENT})
+                }, 1e3);
+            }))}>+</button>
+            <button onClick={() => dispatch5(new Promise(resolve => {
+                setTimeout(() => {
+                    resolve({type: DECREMENT})
+                }, 1e3);
+            }))}>-</button>
         </>
     )
 }
