bdea2c36a59d4ede9bf976f376f21ebd0e338245
luoquanquan <luoquanquan@baidu.com>
2020-09-22 15:44:46 +0800

    simpleInsetanceof2

--- simple-instanceof/index.js
+++ simple-instanceof/index.js
@@ -23,3 +23,20 @@ console.log(simpleInsetanceof(quanquan, Object))  // true
 console.log(simpleInsetanceof(quanquan, Person))  // true
 console.log(simpleInsetanceof(quanquan, Man))     // true
 console.log(simpleInsetanceof(quanquan, Array))   // false
+
+// 由于 __proto__ 是非标准属性, 所以...
+const simpleInsetanceof2 = (sub, sup) => {
+    // 基本数据类型都返回false
+    if (typeof sub !== 'object' || sub === null) return false;
+    let proto = Object.getPrototypeOf(sub);
+    while (true) {
+        if (proto === null) return false;
+        if (proto === sup.prototype) return true;
+        proto = Object.getPrototypeOf(proto);
+    }
+}
+
+console.log(simpleInsetanceof2(quanquan, Object))  // true
+console.log(simpleInsetanceof2(quanquan, Person))  // true
+console.log(simpleInsetanceof2(quanquan, Man))     // true
+console.log(simpleInsetanceof2(quanquan, Array))   // false
