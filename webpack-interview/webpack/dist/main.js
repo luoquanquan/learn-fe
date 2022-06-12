2f04be15118f5c9d9efc9dc8d4c63353dea19eaa
罗圈圈 <luoquanquan@sinochem.com>
2022-06-12 17:51:31 +0800

    webpack

---
+++ webpack-interview/webpack/dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var func = function func() {
+  console.log('hello world ~');
+};
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
