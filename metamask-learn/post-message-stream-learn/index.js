1b4ab2897dcf4da82e09055e54da583c57ffc29f
quanquan.luo <quanquan.luo@okg.com>
2022-09-26 16:55:16 +0800

    post-message-stream 示例

--- metamask-learn/post-message-stream-learn/index.js
+++ metamask-learn/post-message-stream-learn/index.js
@@ -1,9 +1,32 @@
 import $ from 'jquery'
+import { WindowPostMessageStream } from '@metamask/post-message-stream'
 
 let fooPage
+let barPage
+
+const registerBarStream = () => {
+    const indexStream = new WindowPostMessageStream({
+        name: 'indexStream',
+        target: 'barStream',
+        targetWindow: barPage
+    })
+
+    indexStream.on('data', data => {
+        console.log(`${Date.now()} receive data via stream: `, data)
+    })
+
+    $('#btn-hi-via-stream').on('click', () => {
+        indexStream.write('hi this is Home via stream')
+    })
+}
 
 $('#btn-open-foo').on('click', () => {
     fooPage = window.open('/sub-html/foo.html')
+})
+
+$('#btn-open-bar').on('click', () => {
+    barPage = window.open('/sub-html/bar.html')
+    registerBarStream()
 })
 
 $('#btn-hi-via-post-message').on('click', () => {
@@ -12,6 +35,6 @@ $('#btn-hi-via-post-message').on('click', () => {
     }
 })
 
-window.addEventListener('message', ({data}) => {
-    console.log(`Current timestamp ${Date.now()} data: `, data)
-})
+// window.addEventListener('message', ({data}) => {
+//     console.log(`Current timestamp ${Date.now()} data: `, data)
+// })
