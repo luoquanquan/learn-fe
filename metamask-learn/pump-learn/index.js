e177716b6ca49dd0b1594c93bb25774e1d79e256
quanquan.luo <quanquan.luo@okg.com>
2022-10-03 13:09:06 +0800

    pump

--- metamask-learn/pump-learn/index.js
+++ metamask-learn/pump-learn/index.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
+const pump = require('pump')
 
 const readStream = fs.createReadStream('./index.js')
 const writeStream = fs.createWriteStream('./dest')
 
 setTimeout(() => {
     writeStream.destroy()
-    readStream.pipe(writeStream)
+
+    pump(readStream, writeStream, err => {
+        if (err) {
+            console.log('发生错误, 写入失败 ~')
+            return
+        }
+
+        console.log('写入完成')
+    })
 }, 1e3)
