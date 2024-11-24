09aa8882c7f801f1e5ee137a23617be8c168c206
quanquan.luo <quanquan.luo@okg.com>
2024-11-24 20:16:05 +0800

    [206] 反转链表

---
+++ algorithm/leetcode/206.反转链表.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    let i = head
+    let j = null
+
+    while (i) {
+        const tempNext = i.next
+        i.next = j
+        j = i
+        i = tempNext
+    }
+
+    return j
+}
+// @lc code=end
