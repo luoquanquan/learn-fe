1b633f11ff9a5777a14e2dbe74c8d5d3ef99fc85
quanquan.luo <quanquan.luo@okg.com>
2024-11-26 23:29:46 +0800

    广度优先遍历

---
+++ algorithm/jsInterview/chapter8/tree/bfs.js
@@ -0,0 +1,44 @@
+const tree = {
+    val: 'a',
+    children: [
+        {
+            val: 'b',
+            children: [
+                {
+                    val: 'd',
+                    children: []
+                },
+                {
+                    val: 'e',
+                    children: []
+                }
+            ]
+        },
+        {
+            val: 'c',
+            children: [
+                {
+                    val: 'f',
+                    children: []
+                },
+                {
+                    val: 'g',
+                    children: []
+                }
+            ]
+        }
+    ]
+}
+
+// 广度优先遍历
+const bfs = (root) => {
+    const queue = [root]
+
+    while (queue.length) {
+        const head = queue.shift()
+        console.log(head.val)
+        head.children.forEach(ele => queue.push(ele))
+    }
+}
+
+bfs(tree)
