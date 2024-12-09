dcf049eee0149af94e9e37f94e8df81922cf1d9c
quanquan.luo <quanquan.luo@okg.com>
2024-12-09 14:56:16 +0800

    update code sturcture

--- js/extends/es5.js
+++ js/extends/es5.js
@@ -1,39 +1,39 @@
-function Animal() {
-    if (!(this instanceof Animal)) {
-        throw new Error('the constructor must called with new~')
-    }
+function Animal () {
+  if (!(this instanceof Animal)) {
+    throw new Error('the constructor must called with new~')
+  }
 
-    this.name = 'Animal'
+  this.name = 'Animal'
 }
 
-function defineProperty(Constructor, protoProperties) {
-    if(Array.isArray(protoProperties)) {
-        protoProperties.forEach(({key, value}) => {
-            Object.defineProperty(Constructor.prototype, key, {
-                configurable: true,
-                // 模拟 es6 的 class 中的原型方法不可枚举
-                enumerable: false,
-                value
-            })
-        })
-    }
-
+function defineProperty (Constructor, protoProperties) {
+  if (Array.isArray(protoProperties)) {
+    protoProperties.forEach(({ key, value }) => {
+      Object.defineProperty(Constructor.prototype, key, {
+        configurable: true,
+        // 模拟 es6 的 class 中的原型方法不可枚举
+        enumerable: false,
+        value
+      })
+    })
+  }
 }
 
 defineProperty(Animal, [
-    {
-        key: 'eat',
-        value: function() {
-            console.log('i can eat~')
-        }
-    },
-    {
-        key: 'say',
-        value: function() {
-            console.log('i can say~')
-        }
+  {
+    key: 'eat',
+    value: function () {
+      console.log('i can eat~')
     }
+  },
+  {
+    key: 'say',
+    value: function () {
+      console.log('i can say~')
+    }
+  }
 ])
 
+// eslint-disable-next-line no-unused-vars
 const animal = new Animal()
 console.log(Animal.prototype)
